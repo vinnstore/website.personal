1eadf6c9da0756cc7df9037e5a4aa14798bbf943
vinnstore <126059774+vinnstore@users.noreply.github.com>
2024-10-26 20:25:13 +0700

    Add files via upload

---
+++ src/constants/images.js
@@ -0,0 +1,51 @@
+import Img1 from "../assets/pics/90.jpg";
+import Img2 from "../assets/pics/91.jpg";
+import Img3 from "../assets/pics/92.jpg";
+import Img4 from "../assets/pics/93.jpg";
+import Img5 from "../assets/pics/94.jpg";
+import Img6 from "../assets/pics/100.jpg";
+import Img7 from "../assets/pics/96.jpg";
+import Img8 from "../assets/pics/97.jpg";
+import Img9 from "../assets/pics/98.jpg";
+import Img10 from "../assets/pics/99.jpg";
+import Img11 from "../assets/pics/101.jpg";
+import Img12 from "../assets/pics/102.jpg";
+
+export const images = [
+  {
+    src: Img1,
+  },
+  {
+    src: Img2,
+  },
+  {
+    src: Img3,
+  },
+  {
+    src: Img4,
+  },
+  {
+    src: Img5,
+  },
+  {
+    src: Img6,
+  },
+  {
+    src: Img7,
+  },
+  {
+    src: Img8,
+  },
+  {
+    src: Img9,
+  },
+  {
+    src: Img10,
+  },
+  {
+    src: Img11,
+  },
+  {
+    src: Img12,
+  },
+];
